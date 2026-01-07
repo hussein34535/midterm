17b734fd015d05c34ec7deb1c99431fb9384fa92
hussein34535 <hussein34535@proton.me>
2026-01-07 06:51:01 +0200

    ؤلا

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -5,7 +5,9 @@ const sendEmail = async (to, subject, html) => {
         // Check if Gmail credentials exist
         if (process.env.EMAIL_USER && process.env.EMAIL_PASS) {
             const transporter = nodemailer.createTransport({
-                service: 'gmail',
+                host: 'smtp.gmail.com',
+                port: 465,
+                secure: true, // use SSL
                 auth: {
                     user: process.env.EMAIL_USER,
                     pass: process.env.EMAIL_PASS
